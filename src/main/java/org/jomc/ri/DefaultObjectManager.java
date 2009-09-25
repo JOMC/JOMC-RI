6129f06dd6335dbce23c80df70eed1489050b2fc
Christian Schulte <cs@schulte.it>
2009-09-25 22:07:15 +0000

    [maven-scm-plugin] Build 2009-09-25T22:06:44+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ri@592 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/ri/DefaultObjectManager.java
+++ src/main/java/org/jomc/ri/DefaultObjectManager.java
@@ -96,7 +96,7 @@ import org.xml.sax.SAXException;
 // SECTION-END
 // SECTION-START[Annotations]
 @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-2-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-3-SNAPSHOT/jomc-tools" )
 // SECTION-END
 public class DefaultObjectManager implements ObjectManager
 {
@@ -104,7 +104,7 @@ public class DefaultObjectManager implements ObjectManager
 
     /** Creates a new {@code DefaultObjectManager} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-2-SNAPSHOT/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-3-SNAPSHOT/jomc-tools" )
     public DefaultObjectManager()
     {
         // SECTION-START[Default Constructor]
