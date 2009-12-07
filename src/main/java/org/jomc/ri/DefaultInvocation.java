203ba45db570629040cc8fb678b31a1a70eb2f4b
Christian Schulte <cs@schulte.it>
2009-12-07 03:07:51 +0000

    [maven-scm-plugin] Build 2009-12-07T03:07:08+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ri@1106 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/ri/DefaultInvocation.java
+++ src/main/java/org/jomc/ri/DefaultInvocation.java
@@ -57,7 +57,7 @@ import org.jomc.spi.Invocation;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-11-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-12-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class DefaultInvocation implements Invocation
@@ -184,7 +184,7 @@ public class DefaultInvocation implements Invocation
 
     /** Creates a new {@code DefaultInvocation} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-11-SNAPSHOT/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-12-SNAPSHOT/jomc-tools" )
     public DefaultInvocation()
     {
         // SECTION-START[Default Constructor]
