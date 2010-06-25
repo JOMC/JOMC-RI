7f5428a94d54b975cd192b684a8fe75a479ba647
Christian Schulte <cs@schulte.it>
2010-06-25 03:22:09 +0000

    o Moved the 'org.jomc.model.Modules.MODEL_PUBLIC_ID' constant to 'org.jomc.model.ModelObject.MODEL_PUBLIC_ID'. o Added a 'model' parameter to the 'maven-jomc-plugin' and class 'org.jomc.mojo.JomcResourceTransformer'. o Added a 'model' option to the command line application.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ri@2124 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/ri/DefaultObjectManager.java
+++ src/main/java/org/jomc/ri/DefaultObjectManager.java
@@ -65,6 +65,7 @@ import org.jomc.model.ImplementationReference;
 import org.jomc.model.Implementations;
 import org.jomc.model.Instance;
 import org.jomc.model.Message;
+import org.jomc.model.ModelObject;
 import org.jomc.model.Module;
 import org.jomc.model.Modules;
 import org.jomc.model.Multiplicity;
@@ -92,20 +93,20 @@ import org.jomc.util.WeakIdentityHashMap;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getDefaultInvokerInfoMessage defaultInvokerInfoMessage}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 for ''{0}''.</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 f&uuml;r ''{0}'' registriert.</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 for ''{0}''.</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 f&uuml;r ''{0}'' registriert.</pre></td></tr>
  * </table>
  * <li>"{@link #getDefaultLocatorInfoMessage defaultLocatorInfoMessage}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scheme ''{0}'' for ''{1}''.</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scheme ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scheme ''{0}'' for ''{1}''.</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scheme ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
  * </table>
  * <li>"{@link #getDefaultLogLevelInfoMessage defaultLogLevelInfoMessage}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>{0} default log level: ''{1}''</pre></td></tr>
  * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>{0} Standard-Protokollierungsstufe: ''{1}''</pre></td></tr>
  * </table>
  * <li>"{@link #getDefaultScopeInfoMessage defaultScopeInfoMessage}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scope ''{0}'' for ''{1}''.</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scope ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scope ''{0}'' for ''{1}''.</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scope ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
  * </table>
  * <li>"{@link #getDependencyCycleMessage dependencyCycleMessage}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>A dependency of implementation ''{0}'' introduces a cycle.</pre></td></tr>
@@ -128,8 +129,8 @@ import org.jomc.util.WeakIdentityHashMap;
  * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>Ung&uuml;ltige ''Object''-Spezifikation ''{0}''. Kardinalit&auml;t ''{1}''.</pre></td></tr>
  * </table>
  * <li>"{@link #getImplementationInfoMessage implementationInfoMessage}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 initialized in {0,number}ms.</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 in {0,number}ms initialisiert.</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 initialized in {0,number}ms.</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 in {0,number}ms initialisiert.</pre></td></tr>
  * </table>
  * <li>"{@link #getInvokerInfoMessage invokerInfoMessage}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Registered invoker implementation ''{0}'' for ''{1}''.</pre></td></tr>
@@ -1302,8 +1303,8 @@ public class DefaultObjectManager implements ObjectManager
 
                     } );
 
-                    Model model = modelContext.findModel( Modules.MODEL_PUBLIC_ID );
-                    JAXBElement<Modules> e = model.getAnyElement( Modules.MODEL_PUBLIC_ID, "modules" );
+                    Model model = modelContext.findModel( ModelObject.MODEL_PUBLIC_ID );
+                    JAXBElement<Modules> e = model.getAnyElement( ModelObject.MODEL_PUBLIC_ID, "modules" );
 
                     if ( e != null )
                     {
@@ -1326,7 +1327,7 @@ public class DefaultObjectManager implements ObjectManager
 
                         if ( validationReport.isModelValid() )
                         {
-                            e = model.getAnyElement( Modules.MODEL_PUBLIC_ID, "modules" );
+                            e = model.getAnyElement( ModelObject.MODEL_PUBLIC_ID, "modules" );
 
                             if ( e != null )
                             {
@@ -2519,8 +2520,8 @@ public class DefaultObjectManager implements ObjectManager
     /**
      * Gets the text of the {@code defaultInvokerInfoMessage} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 for ''{0}''.</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 f&uuml;r ''{0}'' registriert.</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 for ''{0}''.</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultInvoker Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 f&uuml;r ''{0}'' registriert.</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @param classLoaderInfo Format argument.
@@ -2559,8 +2560,8 @@ public class DefaultObjectManager implements ObjectManager
     /**
      * Gets the text of the {@code defaultLocatorInfoMessage} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scheme ''{0}'' for ''{1}''.</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scheme ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scheme ''{0}'' for ''{1}''.</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultLocator Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scheme ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @param schemeInfo Format argument.
@@ -2641,8 +2642,8 @@ public class DefaultObjectManager implements ObjectManager
     /**
      * Gets the text of the {@code defaultScopeInfoMessage} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scope ''{0}'' for ''{1}''.</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 Scope ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>Registered DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scope ''{0}'' for ''{1}''.</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultScope Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 Scope ''{0}'' f&uuml;r ''{1}'' registriert.</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @param scopeIdentifier Format argument.
@@ -2884,8 +2885,8 @@ public class DefaultObjectManager implements ObjectManager
     /**
      * Gets the text of the {@code implementationInfoMessage} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 initialized in {0,number}ms.</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T03:43:35+0200 in {0,number}ms initialisiert.</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 initialized in {0,number}ms.</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>DefaultObjectManager Version 1.0-beta-5-SNAPSHOT Build 2010-06-25T05:10:16+0200 in {0,number}ms initialisiert.</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @param initializationMillis Format argument.
