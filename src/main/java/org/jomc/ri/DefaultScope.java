5c7782abf05fb270e05e1994fecbfd0df17442ff
Christian Schulte <cs@schulte.it>
2010-07-07 16:01:46 +0000

    [maven-scm-plugin] Build 2010-07-07T18:01:22+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ri@2494 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/ri/DefaultScope.java
+++ src/main/java/org/jomc/ri/DefaultScope.java
@@ -51,7 +51,7 @@ import org.jomc.spi.Scope;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.1/jomc-tools" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.1.x/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class DefaultScope implements Scope
@@ -111,7 +111,7 @@ public class DefaultScope implements Scope
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
 
     /** Creates a new {@code DefaultScope} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.1/jomc-tools" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.1.x/jomc-tools" )
     public DefaultScope()
     {
         // SECTION-START[Default Constructor]
