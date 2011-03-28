ef7ecd034dfb1620fb631d1efac3ee4e7003c294
Christian Schulte <cs@schulte.it>
2011-03-28 15:02:43 +0000

    [maven-scm-plugin] Build 2011-03-28T17:01:50+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ri@3275 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/test/java/org/jomc/ri/test/InvokerTestSpecification.java
+++ src/test/java/org/jomc/ri/test/InvokerTestSpecification.java
@@ -41,23 +41,28 @@ import java.lang.*;
 // SECTION-START[Documentation]
 // <editor-fold defaultstate="collapsed" desc=" Generated Documentation ">
 /**
- *
  * <p>
- *   This specification declares a multiplicity of {@code Many}.
- *   An application assembler may provide multiple implementations of this specification (including none).
- * </p>
- *
- * <p>
- *   Use of class {@link org.jomc.ObjectManager ObjectManager} is supported for accessing implementations.
- *   <pre>
- * InvokerTestSpecification[] objects = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( InvokerTestSpecification[].class );
- * InvokerTestSpecification object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( InvokerTestSpecification.class, "<i>implementation name</i>" );
- *   </pre>
- * </p>
- *
- * <p>
- *   This specification applies to {@code Singleton} scope.
- *   The same singleton object is returned whenever requested.
+ *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
+ *     <tr class="TableHeadingColor">
+ *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Specification</font></th>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.ri.test.InvokerTestSpecification}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Multiplicity:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code Many}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Scope:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code Singleton}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code 1.2-SNAPSHOT}</td>
+ *     </tr>
+ *   </table>
  * </p>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
--- src/test/java/org/jomc/ri/test/TestSpecificationOne.java
+++ src/test/java/org/jomc/ri/test/TestSpecificationOne.java
@@ -41,22 +41,28 @@ import java.lang.*;
 // SECTION-START[Documentation]
 // <editor-fold defaultstate="collapsed" desc=" Generated Documentation ">
 /**
- *
  * <p>
- *   This specification declares a multiplicity of {@code One}.
- *   An application assembler may provide either no or one implementation of this specification.
- * </p>
- *
- * <p>
- *   Use of class {@link org.jomc.ObjectManager ObjectManager} is supported for accessing implementations.
- *   <pre>
- * TestSpecificationOne object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( TestSpecificationOne.class );
- * TestSpecificationOne object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( TestSpecificationOne.class, "<i>implementation name</i>" );
- *   </pre>
- * </p>
- *
- * <p>
- *   This specification does not apply to any scope. A new object is returned whenever requested.
+ *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
+ *     <tr class="TableHeadingColor">
+ *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Specification</font></th>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.ri.test.TestSpecificationOne}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Multiplicity:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code One}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Scope:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code Multiton}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code 1.2-SNAPSHOT}</td>
+ *     </tr>
+ *   </table>
  * </p>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
